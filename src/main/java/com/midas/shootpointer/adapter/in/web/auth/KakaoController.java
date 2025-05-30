9d16935fcb5c31a836c5a9597232591e386b29ed
Park Jae Seong <timer973@naver.com>
2025-05-30 22:36:28 +0900

    🌟 Feature#6 : Kakao Login 구현 / 이메일, 이름 저장

---
+++ src/main/java/com/midas/shootpointer/adapter/in/web/auth/KakaoController.java
@@ -0,0 +1,28 @@
+package com.midas.shootpointer.adapter.in.web.auth;
+
+import com.midas.shootpointer.adapter.in.web.auth.dto.KakaoDTO;
+import com.midas.shootpointer.application.member.service.KakaoService;
+import com.midas.shootpointer.domain.member.MsgEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("kakao")
+public class KakaoController {
+
+    private final KakaoService kakaoService;
+
+    @GetMapping("/callback")
+    public ResponseEntity<MsgEntity> callback(HttpServletRequest request) throws Exception {
+        KakaoDTO kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"));
+
+        return ResponseEntity.ok()
+                .body(new MsgEntity("Success", kakaoInfo));
+        // HTTP 응답을 생성, OK 응답을 반환하며, body로 MsgEntity 객체 담고 있음
+    }
+}
---
+++ src/main/java/com/midas/shootpointer/domain/member/Member.java
@@ -0,0 +1,23 @@
+package com.midas.shootpointer.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "memberId")
+    private Long member_id;
+
+    @Column(name = "member_name")
+    private String username;
+    private String email;
+}
