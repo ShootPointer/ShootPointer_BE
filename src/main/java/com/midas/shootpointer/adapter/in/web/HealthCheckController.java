e78bbdcef9982a17cd1a4f2aae26d1d369d764ba
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-06-05 02:12:47 +0900

    refact : HealthCheckController.java

--- src/main/java/com/midas/shootpointer/adapter/in/web/HealthCheckController.java
+++ src/main/java/com/midas/shootpointer/adapter/in/web/HealthCheckController.java
@@ -1,12 +1,14 @@
 package com.midas.shootpointer.adapter.in.web;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-@RestController("/api")
+
+@RestController
+@RequestMapping("/api")
 public class HealthCheckController {
-     @GetMapping("/health-check")
+    @GetMapping("/health-check")
     public ResponseEntity<String> healthCheck(){
         return ResponseEntity.ok("health-check");
     }
