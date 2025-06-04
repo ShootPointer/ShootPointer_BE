0e21972b4e807978f72e2e44d54b688a56ea9c78
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-06-05 02:07:39 +0900

    Update HealthCheckController.java

--- src/main/java/com/midas/shootpointer/adapter/in/web/HealthCheckController.java
+++ src/main/java/com/midas/shootpointer/adapter/in/web/HealthCheckController.java
@@ -3,7 +3,7 @@ package com.midas.shootpointer.adapter.in.web;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController("/api")
 public class HealthCheckController {
      @GetMapping("/health-check")
