106b1f9b3670afd34e27aca8904052071425a404
tkv00 <tkv0098@gmail.com>
2025-06-15 21:21:13 +0900

    feat : 등번호 서비스 구현체

--- src/main/java/com/midas/shootpointer/domain/backnumber/service/BackNumberServiceImpl.java
+++ src/main/java/com/midas/shootpointer/domain/backnumber/service/BackNumberServiceImpl.java
@@ -2,13 +2,17 @@ package com.midas.shootpointer.domain.backnumber.service;
 
 import com.midas.shootpointer.domain.backnumber.dto.BackNumberRequest;
 import com.midas.shootpointer.domain.backnumber.dto.BackNumberResponse;
+import com.midas.shootpointer.domain.backnumber.repository.BackNumberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BackNumberServiceImpl implements BackNumberService{
+    private final BackNumberRepository repository;
     @Override
+    @Transactional
     public BackNumberResponse create(BackNumberRequest request) {
         return null;
     }
