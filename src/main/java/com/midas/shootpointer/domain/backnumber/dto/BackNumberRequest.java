fcbaa76d7f6a9a9f934c30cc8d3062ef956952cd
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-06-24 03:22:21 +0900

    Update BackNumberRequest.java

--- src/main/java/com/midas/shootpointer/domain/backnumber/dto/BackNumberRequest.java
+++ src/main/java/com/midas/shootpointer/domain/backnumber/dto/BackNumberRequest.java
@@ -2,7 +2,7 @@ package com.midas.shootpointer.domain.backnumber.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
