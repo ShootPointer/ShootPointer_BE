269eaa6a6b8191b3ac7cd5d5a8e0785ca39b0a26
tkv00 <tkv0098@gmail.com>
2025-08-28 17:16:17 +0900

    test : PostHelperImpl test 검증

---
+++ src/test/java/com/midas/shootpointer/domain/post/helper/PostHelperImplTest.java
@@ -0,0 +1,52 @@
+package com.midas.shootpointer.domain.post.helper;
+
+import com.midas.shootpointer.domain.member.entity.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
+class PostHelperImplTest {
+    @InjectMocks
+    private PostHelperImpl postHelper;
+
+    @Mock
+    private PostValidation postValidation;
+
+
+    @Test
+    @DisplayName("유저의 하이라이트 영상인지 확인합니다-postValidation.isValidateHighlightId 메서드가 실행되는지 확인합니다.")
+    void isValidateHighlightId() {
+        //given
+        UUID mockUUID=UUID.randomUUID();
+        Member mockMember=mock(Member.class);
+
+        //when
+        postHelper.isValidateHighlightId(mockMember,mockUUID);
+
+        //then
+        verify(postValidation,times(1)).isValidateHighlightId(any(Member.class),any(UUID.class));
+    }
+
+    @Test
+    @DisplayName("유저의 해시태그가 올바른지 확인합니다-postValidation.isValidPostHashTag 메서드가 실행되는지 확인합니다.")
+    void isValidPostHashTag() {
+        //given
+        Object mockObject=new Object();
+
+        //when
+        postHelper.isValidPostHashTag(mockObject);
+
+        //then
+        verify(postValidation,times(1)).isValidPostHashTag(mockObject);
+    }
+}
