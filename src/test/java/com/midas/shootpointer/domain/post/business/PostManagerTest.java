8a608f1afd501d63da5600fbd017a90cbe728c81
tkv00 <tkv0098@gmail.com>
2025-08-29 15:39:48 +0900

    test : PostManager - update 메서드 test 검증

--- src/test/java/com/midas/shootpointer/domain/post/business/PostManagerTest.java
+++ src/test/java/com/midas/shootpointer/domain/post/business/PostManagerTest.java
@@ -1,5 +1,7 @@
 package com.midas.shootpointer.domain.post.business;
 
+import com.midas.shootpointer.domain.highlight.entity.HighlightEntity;
+import com.midas.shootpointer.domain.highlight.helper.HighlightHelper;
 import com.midas.shootpointer.domain.member.entity.Member;
 import com.midas.shootpointer.domain.post.business.command.PostCommandService;
 import com.midas.shootpointer.domain.post.business.command.PostCommandServiceImpl;
@@ -9,6 +11,7 @@ import com.midas.shootpointer.domain.post.entity.PostEntity;
 import com.midas.shootpointer.domain.post.helper.PostHelper;
 import com.midas.shootpointer.domain.post.mapper.PostMapper;
 import com.midas.shootpointer.domain.post.repository.PostCommandRepository;
+import com.midas.shootpointer.domain.post.repository.PostQueryRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +23,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
@@ -35,7 +39,13 @@ class PostManagerTest {
     private PostHelper postHelper;
 
     @Mock
+    private HighlightHelper highlightHelper;
+
+    @Mock
     private PostCommandRepository postCommandRepository;
+
+    @Mock
+    private PostQueryRepository postQueryRepository;
 
     @Test
     @DisplayName(
@@ -48,7 +58,9 @@ class PostManagerTest {
         Member mockMember=mockMember();
         PostEntity mockPostEntity=mockPostEntity();
         UUID randomUUID=UUID.randomUUID();
+        HighlightEntity mockHighlight=mockHighlight();
 
+        when(highlightHelper.findHighlightByHighlightId(randomUUID)).thenReturn(mockHighlight);
         doNothing().when(postHelper).isValidateHighlightId(mockMember,randomUUID);
         doNothing().when(postHelper).isValidPostHashTag(mockPostEntity.getHashTag());
         when(postCommandRepository.save(mockPostEntity))
@@ -65,8 +77,57 @@ class PostManagerTest {
 
         //then
         assertThat(savedPostId).isEqualTo(111L);
+        verify(highlightHelper,times(1)).findHighlightByHighlightId(randomUUID);
         verify(postHelper,times(1)).isValidateHighlightId(mockMember,randomUUID);
         verify(postHelper,times(1)).isValidPostHashTag(mockPostEntity.getHashTag());
+    }
+
+    @Test
+    @DisplayName("postHelper의 다양한 유효성 검증을 진행하고 게시물을 수정하고 성공 시 postId를 반환합니다.")
+    void update(){
+        Member mockMember = mockMember();
+        PostEntity mockPostEntity = spy(mockPostEntity());
+        UUID highlightId = UUID.randomUUID();
+        PostRequest postRequest = PostRequest.of(highlightId, "title2", "content2", HashTag.TWO_POINT);
+        HighlightEntity mockHighlight = mockHighlight();
+        Long postId = 111L;
+
+        when(postQueryRepository.findByPostId(postId)).thenReturn(Optional.of(mockPostEntity));
+        doNothing().when(postHelper).isDeleted(mockPostEntity);
+        doNothing().when(postHelper).isMembersPost(mockPostEntity, mockMember);
+        when(highlightHelper.findHighlightByHighlightId(highlightId)).thenReturn(mockHighlight);
+        doNothing().when(postHelper).isValidateHighlightId(mockMember, highlightId);
+        doNothing().when(postHelper).isValidPostHashTag(postRequest.getHashTag());
+
+        when(postCommandRepository.save(any(PostEntity.class))).thenReturn(mockPostEntity);
+
+        //when
+        Long updatedPostId = postManager.update(postRequest, mockMember, postId);
+
+        //then
+        assertThat(updatedPostId).isEqualTo(mockPostEntity.getPostId());
+        verify(postQueryRepository, times(1)).findByPostId(postId);
+        verify(postHelper, times(1)).isDeleted(mockPostEntity);
+        verify(postHelper, times(1)).isMembersPost(mockPostEntity, mockMember);
+        verify(postHelper, times(1)).isValidateHighlightId(mockMember, highlightId);
+        verify(postHelper, times(1)).isValidPostHashTag(postRequest.getHashTag());
+        verify(mockPostEntity, times(1)).update(
+                postRequest.getTitle(),
+                postRequest.getContent(),
+                postRequest.getHashTag(),
+                mockHighlight
+        );
+        verify(postCommandRepository, times(1)).save(mockPostEntity);
+    }
+    /**
+     * mock 하이라이트 영상
+     * @return HighlightEntity
+     */
+    private HighlightEntity mockHighlight() {
+        return HighlightEntity.builder()
+                .highlightURL("test")
+                .highlightKey(UUID.randomUUID())
+                .build();
     }
 
     /**
@@ -76,6 +137,15 @@ class PostManagerTest {
     private PostRequest mockPostRequest(){
         return PostRequest.of(UUID.randomUUID(),"title","content", HashTag.TREE_POINT);
     }
+
+    /**
+     * mock 수정 요청 dto 생성
+     * @return PostRequest
+     */
+    private PostRequest mockUpdatePostRequest(){
+        return PostRequest.of(UUID.randomUUID(),"title2","content2", HashTag.TWO_POINT);
+    }
+
 
     /**
      * mock 게시물 엔티티
