4d378be43370a0af37f2bb67885b9f6b01a174fd
tkv00 <tkv0098@gmail.com>
2025-08-29 10:10:58 +0900

    test : PostCommandServiceImpl - create 메서드 test 검증

--- src/main/java/com/midas/shootpointer/domain/post/mapper/PostMapperImpl.java
+++ src/main/java/com/midas/shootpointer/domain/post/mapper/PostMapperImpl.java
@@ -2,7 +2,6 @@ package com.midas.shootpointer.domain.post.mapper;
 
 import com.midas.shootpointer.domain.member.entity.Member;
 import com.midas.shootpointer.domain.post.dto.PostRequest;
-import com.midas.shootpointer.domain.post.dto.PostResponse;
 import com.midas.shootpointer.domain.post.entity.PostEntity;
 import org.springframework.stereotype.Component;
 
--- src/test/java/com/midas/shootpointer/domain/post/business/command/PostCommandServiceImplTest.java
+++ src/test/java/com/midas/shootpointer/domain/post/business/command/PostCommandServiceImplTest.java
@@ -1,12 +1,82 @@
 package com.midas.shootpointer.domain.post.business.command;
 
+import com.midas.shootpointer.domain.member.entity.Member;
+import com.midas.shootpointer.domain.post.business.PostManager;
+import com.midas.shootpointer.domain.post.dto.PostRequest;
+import com.midas.shootpointer.domain.post.entity.HashTag;
+import com.midas.shootpointer.domain.post.entity.PostEntity;
+import com.midas.shootpointer.domain.post.mapper.PostMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class PostCommandServiceImplTest {
+    @InjectMocks
+    private PostCommandServiceImpl postCommandService;
+
+    @Mock
+    private PostManager postManager;
+
+    @Mock
+    private PostMapper postMapper;
+
+    @Test
+    @DisplayName("게시물 저장 시 PostDto->PostEntity 호출 여부를 확인하고 postManager-save 호출 여부를 확인합니다.")
+    void create(){
+        //given
+        PostRequest request=mockPostRequest();
+        PostEntity postEntity=mockPostEntity();
+        Member member=mockMember();
+
+        //when
+        when(postMapper.dtoToEntity(request,member)).thenReturn(postEntity);
+        when(postManager.save(member,postEntity,request.getHighlightId())).thenReturn(111L);
+
+        //then
+        Long createdPostId=postCommandService.create(request,member);
+        verify(postMapper,times(1)).dtoToEntity(request,member);
+        verify(postManager,times(1)).save(member,postEntity,request.getHighlightId());
+        assertThat(createdPostId).isEqualTo(111L);
+    }
+
+    /**
+     * mock Member
+     */
+    private PostRequest mockPostRequest(){
+        return PostRequest.of(UUID.randomUUID(),"title","content", HashTag.TREE_POINT);
+    }
+    /**
+     * mock postEntity
+     */
+    private PostEntity mockPostEntity(){
+        return PostEntity.builder()
+                .title("title")
+                .content("content")
+                .hashTag(HashTag.TREE_POINT)
+                .build();
+    }
+
+    /**
+     * mock PostRequest
+     */
+    private Member mockMember() {
+        return Member.builder()
+                .memberId(UUID.randomUUID())
+                .email("test@naver.com")
+                .username("test")
+                .build();
+    }
 
 }
