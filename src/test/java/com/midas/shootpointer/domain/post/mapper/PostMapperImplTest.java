d78177921f2d85c097efd557cad517c959464d84
tkv00 <tkv0098@gmail.com>
2025-09-01 13:45:59 +0900

    refact : PostMapperImpl @ActiveProfiles 설정

--- src/test/java/com/midas/shootpointer/domain/post/mapper/PostMapperImplTest.java
+++ src/test/java/com/midas/shootpointer/domain/post/mapper/PostMapperImplTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@ActiveProfiles("test")
 class PostMapperImplTest {
     @Autowired
     private PostMapperImpl postMapper;
