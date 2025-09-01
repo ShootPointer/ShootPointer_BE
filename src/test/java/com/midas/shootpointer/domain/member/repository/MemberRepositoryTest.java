7be85a71bad1df286b9e262e502da0d6716fe6f4
tkv00 <tkv0098@gmail.com>
2025-09-01 12:52:42 +0900

    test : MemberRepositortyTest @ActiveProfiles(test) 추가

--- src/test/java/com/midas/shootpointer/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/midas/shootpointer/domain/member/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryTest {
 
