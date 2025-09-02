8fde7bedcde305f43b04bbcd3178b6d23f929337
tkv00 <tkv0098@gmail.com>
2025-09-02 16:23:26 +0900

    refact : @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 추가

--- src/test/java/com/midas/shootpointer/domain/like/helper/LikeUtilImplTest.java
+++ src/test/java/com/midas/shootpointer/domain/like/helper/LikeUtilImplTest.java
@@ -19,6 +19,7 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -29,6 +30,7 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class LikeUtilImplTest {
     @Autowired
     private LikeUtilImpl likeUtil;
