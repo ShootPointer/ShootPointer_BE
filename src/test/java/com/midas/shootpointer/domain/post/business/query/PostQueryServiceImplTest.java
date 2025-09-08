074dc6aef744fa8fda9a3526234c0956e0233445
tkv00 <tkv0098@gmail.com>
2025-09-08 17:44:34 +0900

    test : PostQueryServiceImpl -singleRead() 테스트 검증 #82

---
+++ src/test/java/com/midas/shootpointer/domain/post/business/query/PostQueryServiceImplTest.java
@@ -0,0 +1,61 @@
+package com.midas.shootpointer.domain.post.business.query;
+
+import com.midas.shootpointer.domain.highlight.entity.HighlightEntity;
+import com.midas.shootpointer.domain.member.entity.Member;
+import com.midas.shootpointer.domain.post.business.PostManager;
+import com.midas.shootpointer.domain.post.dto.PostResponse;
+import com.midas.shootpointer.domain.post.entity.HashTag;
+import com.midas.shootpointer.domain.post.entity.PostEntity;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PostQueryServiceImplTest {
+    @InjectMocks
+    private PostQueryServiceImpl postQueryService;
+
+    @Mock
+    private PostManager postManager;
+
+    @Test
+    @DisplayName("게시물 조회 시 postManager - singleRead(postId)의 호출을 확인합니다.")
+    void singleRead(){
+        //given
+        Long postId=123124L;
+        LocalDateTime time=LocalDateTime.now();
+        PostResponse postResponse=makePostResponse(time,postId);
+
+        //when
+        when(postManager.singleRead(postId)).thenReturn(postResponse);
+        postQueryService.singleRead(postId);
+
+        //then
+        verify(postManager,times(1)).singleRead(postId);
+    }
+
+    private PostResponse makePostResponse(LocalDateTime time,Long postId){
+        return PostResponse.builder()
+                .content("content")
+                .likeCnt(10)
+                .createdAt(time)
+                .modifiedAt(time)
+                .highlightUrl("test")
+                .postId(postId)
+                .title("title")
+                .hashTag(HashTag.TWO_POINT)
+                .build();
+
+    }
+}
