552d20513e77bb7d0419c5e20b1811a6b88c7e4e
tkv00 <tkv0098@gmail.com>
2025-09-10 16:23:03 +0900

    feat : 최신순 / 좋아요순 여러 게시물 조회 쿼리 구현 #89

--- src/main/java/com/midas/shootpointer/domain/post/repository/PostQueryRepository.java
+++ src/main/java/com/midas/shootpointer/domain/post/repository/PostQueryRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface PostQueryRepository extends JpaRepository<PostEntity,Long> {
@@ -20,4 +21,23 @@ public interface PostQueryRepository extends JpaRepository<PostEntity,Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = "SELECT p FROM PostEntity AS p WHERE p.postId=:postId")
     PostEntity findByPostIdWithPessimisticLock(@Param(value = "postId")Long postId);
+
+    /**
+     * 최신순 게시물 다건 조회
+     */
+    @Query(value = "SELECT p FROM post as p WHERE post_id < :lastPostId ORDER BY post_id DESC LIMIT :size ",nativeQuery = true)
+    List<PostEntity> getLatestPostListBySliceAndNoOffset (@Param(value = "lastPostId")Long lastPostId,@Param(value = "size")int size);
+
+    /**
+     * 좋아요순 게시물 다건 조회
+     * 좋아요가 같은 경우는 최신 순 봔환.
+     */
+    @Query(value = "SELECT p FROM post as p " +
+                   "WHERE (like_cnt < :likeCnt) OR " +
+                   "(like_cnt = :likeCnt AND post_id < :postId) " +
+                   "ORDER BY like_cnt DESC " +
+                   "LIMIT :size",
+            nativeQuery = true
+    )
+    List<PostEntity> getPopularPostListBySliceAndNoOffset(@Param(value = "lastPostId")Long lastPostId,@Param(value = "size")int size,@Param(value = "likeCnt")int likeCnt);
 }
