a9b059b68b9ae28fe317197e8e5c14d4afd5aea8
tkv00 <tkv0098@gmail.com>
2025-09-10 15:23:33 +0900

    feat : 게시물 조회 방식 ENUM 생성 #87

---
+++ src/main/java/com/midas/shootpointer/domain/post/business/PostOrderType.java
@@ -0,0 +1,13 @@
+package com.midas.shootpointer.domain.post.business;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PostOrderType {
+    //인기순
+    POPULAR,
+    //최신순
+    LATEST
+}
