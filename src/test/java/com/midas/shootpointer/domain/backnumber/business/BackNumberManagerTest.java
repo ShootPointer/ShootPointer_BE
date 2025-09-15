cc7491f98931163cfdd125e825943cf8b7112dca
tkv00 <tkv0098@gmail.com>
2025-09-15 15:50:46 +0900

    test : BackNumberHelperImpl - findOrElseGetBackNumber 메서드 테스트 검증  #100

---
+++ src/test/java/com/midas/shootpointer/domain/backnumber/business/BackNumberManagerTest.java
@@ -0,0 +1,19 @@
+package com.midas.shootpointer.domain.backnumber.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 통합 테스트로 진행
+ */
+@SpringBootTest
+@ActiveProfiles("dev")
+class BackNumberManagerTest {
+
+    @Test
+    void create() {
+    }
+}
