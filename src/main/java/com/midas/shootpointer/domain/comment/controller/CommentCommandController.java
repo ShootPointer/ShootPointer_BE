c359e117131e88d522b024a1b0d6051aa963b3ba
Park Jae Seong <timer973@naver.com>
2025-09-29 18:53:33 +0900

    fix: CommentController PostEntity 관련 수정 작업

--- src/main/java/com/midas/shootpointer/domain/comment/controller/CommentCommandController.java
+++ src/main/java/com/midas/shootpointer/domain/comment/controller/CommentCommandController.java
@@ -6,6 +6,7 @@ import com.midas.shootpointer.domain.comment.entity.Comment;
 import com.midas.shootpointer.domain.comment.mapper.CommentMapper;
 import com.midas.shootpointer.domain.member.entity.Member;
 import com.midas.shootpointer.domain.post.entity.PostEntity;
+import com.midas.shootpointer.domain.post.helper.PostHelper;
 import com.midas.shootpointer.global.dto.ApiResponse;
 import com.midas.shootpointer.global.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
@@ -22,14 +23,14 @@ public class CommentCommandController {
 	
 	private final CommentCommandService commentCommandService;
 	private final CommentMapper commentMapper;
+	private final PostHelper postHelper;
 	
 	@PostMapping
 	public ResponseEntity<ApiResponse<Long>> create(@RequestBody CommentRequestDto requestDto) {
 		
 		Member member = SecurityUtils.getCurrentMember();
-		PostEntity postEntity = PostEntity.builder()
-			.postId(requestDto.getPostId())
-			.build();
+		
+		PostEntity postEntity = postHelper.findPostByPostId(requestDto.getPostId());
 		Comment comment = commentMapper.dtoToEntity(requestDto, member, postEntity);
 		
 		return ResponseEntity.ok(ApiResponse.created(commentCommandService.create(comment)));
