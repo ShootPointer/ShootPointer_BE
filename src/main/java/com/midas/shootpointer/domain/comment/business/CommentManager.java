54e5982410686600c094fdfcc115cd42ba6b6889
Park Jae Seong <timer973@naver.com>
2025-10-01 10:13:09 +0900

    refact: CommentManager

--- src/main/java/com/midas/shootpointer/domain/comment/business/CommentManager.java
+++ src/main/java/com/midas/shootpointer/domain/comment/business/CommentManager.java
@@ -17,12 +17,7 @@ public class CommentManager {
 	
 	@Transactional
 	public Long save(Comment comment) {
-		
-		//* TODO : 첫 번째 코드와 아래 두 번째 코드가 사실상 같은 역할인데 다른 코드로 구현되어있음.
-		//* 도메인 역할 분리를 위해 따로 구현할 것인지 OR 그냥 하나로 통합할 것인지
 		postHelper.findPostByPostId(comment.getPost().getPostId()); // 게시물이 존재하는지만 확인
-		
-		commentHelper.validatePostExists(comment.getPost().getPostId()); // 댓글 validation 체크
 		
 		return commentHelper.save(comment).getCommentId();
 	}
