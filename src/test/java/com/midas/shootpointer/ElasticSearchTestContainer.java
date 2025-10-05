258e4a516f62a8cd42ce1af5f6ec988d5989f9dc
tkv00 <tkv0098@gmail.com>
2025-10-06 00:21:56 +0900

    refact : ElasticTestConteiner 로드 시간 제한 추가

--- src/test/java/com/midas/shootpointer/ElasticSearchTestContainer.java
+++ src/test/java/com/midas/shootpointer/ElasticSearchTestContainer.java
@@ -4,9 +4,12 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 
 @Profile({"es","test"})
 @Testcontainers
@@ -19,7 +22,8 @@ public class ElasticSearchTestContainer {
             .withEnv("discovery.type", "single-node")
             .withEnv("xpack.security.enabled", "false")
             .withEnv("xpack.security.http.ssl.enabled", "false")
-            .withCommand("sh", "-c", "elasticsearch-plugin install analysis-nori && elasticsearch");
+            .withCommand("sh", "-c", "elasticsearch-plugin install analysis-nori && elasticsearch")
+            .waitingFor(Wait.forHttp("/").forStatusCode(200).withStartupTimeout(Duration.ofSeconds(90)));;
 
 
     @DynamicPropertySource
