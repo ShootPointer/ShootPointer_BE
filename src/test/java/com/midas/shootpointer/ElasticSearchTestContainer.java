669e9da88d24b5dc89c30e78be6bdd63dc13a3ce
tkv00 <tkv0098@gmail.com>
2025-10-06 14:40:15 +0900

    refact : ElasticSearchTestContainer 수정

--- src/test/java/com/midas/shootpointer/ElasticSearchTestContainer.java
+++ src/test/java/com/midas/shootpointer/ElasticSearchTestContainer.java
@@ -1,20 +1,19 @@
 package com.midas.shootpointer;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.time.Duration;
-
 @Profile({"es","test"})
 @Testcontainers
 @TestConfiguration(proxyBeanMethods = false)
+@ConditionalOnExpression("!'{SPRING_ELASTICSEARCH_MODE:local}'.equals('external')")
 public class ElasticSearchTestContainer {
     private static final DockerImageName IMAGE_NAME =
             DockerImageName.parse("tkv00/elasticsearch-nori:7.17.9")
@@ -26,32 +25,18 @@ public class ElasticSearchTestContainer {
             .withEnv("xpack.security.enabled", "false")
             .withEnv("xpack.security.http.ssl.enabled", "false")
             .withEnv("bootstrap.memory_lock", "false")
-            .withEnv("ES_JAVA_OPTS", "-Xms512m -Xmx512m")
-           // .withCommand("sh", "-c", "elasticsearch-plugin install analysis-nori && elasticsearch")
-            .waitingFor(Wait.forHttp("/").forStatusCode(200).withStartupTimeout(Duration.ofSeconds(90)));
+            .withEnv("ES_JAVA_OPTS", "-Xms512m -Xmx512m");
+            //.withCommand("sh", "-c", "elasticsearch-plugin install analysis-nori && elasticsearch");
 
     static {
-        //local 에서만 test container 실행
-        String mode=System.getProperty("spring.elasticsearch.mode",
-                System.getenv().getOrDefault("SPRING_ELASTICSEARCH_MODE", "container"));
-        if("container".equalsIgnoreCase(mode)) {
-            container.start();
-        }
+        container.start();
     }
 
 
 
     @DynamicPropertySource
     static void setElasticsearchProperties(DynamicPropertyRegistry registry){
-        String mode=System.getProperty("spring.elasticsearch.mode",
-                System.getenv().getOrDefault("SPRING_ELASTICSEARCH_MODE", "container"));
-
-        if ("container".equalsIgnoreCase(mode)){
-            registry.add("spring.elasticsearch.uris",container::getHttpHostAddress);
-        }else{
-            //CI 환경 - Jacoco
-            registry.add("spring.elasticsearch.uris",()->"http://localhost:9200");
-        }
+        registry.add("spring.elasticsearch.uris",container::getHttpHostAddress);
     }
 
 }
