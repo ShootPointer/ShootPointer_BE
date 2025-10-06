2392b4f5552d624f8b0171912af6a89f208e3132
tkv00 <tkv0098@gmail.com>
2025-10-07 01:30:15 +0900

    refact : HighlightMapper 구현 #133

---
+++ src/main/java/com/midas/shootpointer/domain/highlight/mapper/HighlightMapperImpl.java
@@ -0,0 +1,32 @@
+package com.midas.shootpointer.domain.highlight.mapper;
+
+import com.midas.shootpointer.domain.highlight.dto.HighlightResponse;
+import com.midas.shootpointer.domain.highlight.dto.HighlightSelectResponse;
+import com.midas.shootpointer.domain.highlight.entity.HighlightEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class HighlightMapperImpl implements HighlightMapper{
+
+    @Override
+    public HighlightSelectResponse entityToResponse(List<UUID> selectedHighlights) {
+        return HighlightSelectResponse.builder()
+                .selectedHighlightIds(selectedHighlights)
+                .build();
+    }
+
+    @Override
+    public HighlightResponse entityToResponse(HighlightEntity highlight) {
+        return HighlightResponse.builder()
+                .createdAt(highlight.getCreatedAt())
+                .highlightId(highlight.getHighlightId())
+                .highlightIdentifier(highlight.getHighlightKey())
+                .highlightUrl(highlight.getHighlightURL())
+                .build();
+    }
+
+
+}
