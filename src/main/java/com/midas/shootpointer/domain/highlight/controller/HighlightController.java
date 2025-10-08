f8dcf6505320177396092261197785d9e3c7fdcc
tkv00 <tkv0098@gmail.com>
2025-10-08 21:35:13 +0900

    test : HighlightController 테스트 검증

--- src/main/java/com/midas/shootpointer/domain/highlight/controller/HighlightController.java
+++ src/main/java/com/midas/shootpointer/domain/highlight/controller/HighlightController.java
@@ -1,17 +1,15 @@
 package com.midas.shootpointer.domain.highlight.controller;
 
+import com.midas.shootpointer.domain.highlight.business.command.HighlightCommandService;
 import com.midas.shootpointer.domain.highlight.dto.HighlightResponse;
 import com.midas.shootpointer.domain.highlight.dto.HighlightSelectRequest;
 import com.midas.shootpointer.domain.highlight.dto.HighlightSelectResponse;
 import com.midas.shootpointer.domain.highlight.dto.UploadHighlight;
-import com.midas.shootpointer.domain.highlight.business.command.HighlightCommandService;
 import com.midas.shootpointer.domain.member.entity.Member;
 import com.midas.shootpointer.global.dto.ApiResponse;
-import com.midas.shootpointer.global.security.CustomUserDetails;
-import com.midas.shootpointer.global.util.jwt.JwtUtil;
+import com.midas.shootpointer.global.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,15 +21,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class HighlightController {
     private final HighlightCommandService highlightCommandService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/select")
     public ResponseEntity<ApiResponse<HighlightSelectResponse>> selectHighlight(
-            @RequestBody HighlightSelectRequest request,
-            @AuthenticationPrincipal
-            CustomUserDetails userDetails
+            @RequestBody HighlightSelectRequest request
     ) {
-        Member member=userDetails.getMember();
+        Member member = SecurityUtils.getCurrentMember();
+
         return ResponseEntity.ok(ApiResponse.ok(highlightCommandService.selectHighlight(request, member)));
     }
 
@@ -41,7 +37,6 @@ public class HighlightController {
             @RequestPart(value = "uploadHighlightDto") UploadHighlight request,
             @RequestPart(value = "highlights") List<MultipartFile> highlights
     ) {
-        //TODO : JWT 유효성 물어봐야함 -> 재성
         return ResponseEntity.ok(ApiResponse.ok(highlightCommandService.uploadHighlights(request,highlights,memberId)));
     }
 }
