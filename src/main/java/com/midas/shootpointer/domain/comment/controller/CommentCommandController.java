cf7c5729624c663c5b34b1b0e28a6a17971449ec
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-17 02:07:16 +0900

    refact : CommentController 수정

--- src/main/java/com/midas/shootpointer/domain/comment/controller/CommentCommandController.java
+++ src/main/java/com/midas/shootpointer/domain/comment/controller/CommentCommandController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.midas.shootpointer.global.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequestMapping("/api/comment")
