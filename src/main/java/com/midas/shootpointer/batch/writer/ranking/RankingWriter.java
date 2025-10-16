eb6bf5b66a62858ad419551a99a75e275d93022e
tkv00 <tkv0098@gmail.com>
2025-10-16 19:36:45 +0900

    feat : RankingWriter 구현 #157 #158

--- src/main/java/com/midas/shootpointer/batch/writer/ranking/RankingWriter.java
+++ src/main/java/com/midas/shootpointer/batch/writer/ranking/RankingWriter.java
@@ -1,14 +1,23 @@
 package com.midas.shootpointer.batch.writer.ranking;
 
-import com.midas.shootpointer.domain.highlight.entity.HighlightEntity;
+import com.midas.shootpointer.domain.ranking.entity.RankingDocument;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RankingWriter implements ItemWriter<HighlightEntity> {
-    @Override
-    public void write(Chunk<? extends HighlightEntity> chunk) throws Exception {
+@RequiredArgsConstructor
+public class RankingWriter implements ItemWriter<RankingDocument> {
 
+    private final MongoTemplate mongoTemplate;
+
+
+    @Override
+    public void write(Chunk<? extends RankingDocument> chunk) throws Exception {
+        for (RankingDocument doc:chunk){
+            mongoTemplate.save(doc);
+        }
     }
 }
