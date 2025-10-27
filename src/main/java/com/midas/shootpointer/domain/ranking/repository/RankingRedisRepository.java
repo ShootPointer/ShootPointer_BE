8ae4b11d09d9a3b65269c66561710bf725fd6d50
tkv00 <tkv0098@gmail.com>
2025-10-27 15:24:24 +0900

    feat : 이번주/이번달 랭킹 조회 객체 삽입 및 조회 구현 #165 #166

---
+++ src/main/java/com/midas/shootpointer/domain/ranking/repository/RankingRedisRepository.java
@@ -0,0 +1,116 @@
+package com.midas.shootpointer.domain.ranking.repository;
+
+import com.midas.shootpointer.domain.ranking.dto.RankingResult;
+import com.midas.shootpointer.domain.ranking.dto.RankingType;
+import com.midas.shootpointer.domain.ranking.entity.RankingEntry;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Repository
+public class RankingRedisRepository {
+    @Value("${spring.data.redis.custom.ranking.key.weekly}")
+    private String weeklyKeyPrefix;
+
+    @Value("${spring.data.redis.custom.ranking.key.monthly}")
+    private String monthlyKeyPrefix;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private ZSetOperations<String, Object> zSetOperations;
+
+    private final int START = 0;
+    private final int END = 9;
+
+    /**
+     * 가중치
+     */
+    private final int TWO_WEIGHT = 1_000;
+    private final int THREE_WEIGHT = 1_000_000;
+
+    @PostConstruct
+    private void init() {
+        zSetOperations = redisTemplate.opsForZSet();
+    }
+
+    /**
+     * 레디스 객체 등록
+     */
+    public void addOrUpdate(RankingType type, int newScore, RankingResult newResult) {
+        //1. key 결정
+        String key = setKey(type);
+
+        //2. 기존 데이터 검색
+        Set<ZSetOperations.TypedTuple<Object>> existedData=zSetOperations.rangeWithScores(key,0,-1);
+
+        if (existedData!=null){
+            for (ZSetOperations.TypedTuple<Object> tuple:existedData){
+                Object value=tuple.getValue();
+                if (value instanceof RankingResult existingResult){
+                    //3. 기존 데이터가 존재하는 경우
+                    if (existingResult.memberId().equals(newResult.memberId())){
+                        //4. 기존 데이터 삭제
+                        zSetOperations.remove(key,existedData);
+                        break;
+                    }
+                }
+            }
+        }
+
+        //5. 새로운 데이터 삽입
+        zSetOperations.add(key,newResult,newScore);
+    }
+
+    /**
+     * 이번 주 top10 랭킹 조회
+     */
+    public List<RankingEntry> getHighlightsWeeklyRanking(RankingType type) {
+        String key=setKey(type);
+
+
+        List<RankingEntry> results = new ArrayList<>();
+
+        //이번 주 랭킹
+        Set<ZSetOperations.TypedTuple<Object>> rankingSet = zSetOperations.reverseRangeWithScores(key, START, END);
+
+        //값이 비어 있는 경우
+        if (rankingSet == null || rankingSet.isEmpty()) return results;
+
+        int rank = 1;
+
+        for (ZSetOperations.TypedTuple<Object> tuple : rankingSet) {
+            Object value=tuple.getValue();
+            if (value instanceof RankingEntry result){
+                results.add( RankingEntry.builder()
+                        .memberId(result.getMemberId())
+                        .totalScore(result.getTotalScore())
+                        .twoScore(result.getTwoScore())
+                        .threeScore(result.getThreeScore())
+                        .memberName(result.getMemberName())
+                        .rank(rank)
+                        .build()
+                );
+                rank++;
+            }
+        }
+        return results;
+    }
+
+    private String setKey(RankingType type){
+        switch (type){
+            case MONTHLY -> {
+                return monthlyKeyPrefix;
+            }
+            case WEEKLY -> {
+                return weeklyKeyPrefix;
+            }
+        }
+    }
+}
