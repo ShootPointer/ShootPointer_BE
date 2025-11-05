33f31a7afa3c379450bfdab3f1cfd9a5f10f7d0e
tkv00 <tkv0098@gmail.com>
2025-11-05 14:36:29 +0900

    refact : 기존 Highlight 컨트롤러에서 서비스 계층 이용 -> manager 주입 변경

---
+++ src/main/java/com/midas/shootpointer/domain/highlight/controller/HighlightQueryController.java
@@ -0,0 +1,12 @@
+package com.midas.shootpointer.domain.highlight.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/highlight")
+public class HighlightQueryController {
+    private HighlightQue
+}
