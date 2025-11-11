1c9f1bb84504f291fb48ae4a2adf6b706c769fbe
Park Jae Seong <timer973@naver.com>
2025-11-12 03:24:24 +0900

    refact: FCM 기능 CQRS 패턴으로 리팩토링 및 구조 개선

--- src/main/java/com/midas/shootpointer/domain/notification/controller/FcmCommandController.java
+++ src/main/java/com/midas/shootpointer/domain/notification/controller/FcmCommandController.java
@@ -3,9 +3,9 @@ package com.midas.shootpointer.domain.notification.controller;
 import com.midas.shootpointer.domain.notification.business.command.FcmCommandService;
 import com.midas.shootpointer.domain.notification.dto.request.FcmTokenRequestDto;
 import com.midas.shootpointer.domain.notification.dto.request.SendNotificationRequestDto;
+import com.midas.shootpointer.global.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,27 +26,27 @@ public class FcmCommandController {
 	@Operation(
 		summary = "FCM 토큰 등록 API",
 		responses = {
-			@ApiResponse(responseCode = "200", description = "FCM 토큰 등록 성공"),
-			@ApiResponse(responseCode = "4XX", description = "FCM 토큰 등록 실패", content = @Content(mediaType = "application/json"))
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "FCM 토큰 등록 성공"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "4XX", description = "FCM 토큰 등록 실패", content = @Content(mediaType = "application/json"))
 		}
 	)
 	@PostMapping("/register/token")
 	public ResponseEntity<?> registerToken(@Valid @RequestBody FcmTokenRequestDto requestDto) {
 		fcmCommandService.registerToken(requestDto);
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok(ApiResponse.ok(requestDto));
 	}
 	
 	@Operation(
 		summary = "알림 전송 API",
 		responses = {
-			@ApiResponse(responseCode = "200", description = "알림 전송 성공"),
-			@ApiResponse(responseCode = "4xx", description = "알림 전송 실패", content = @Content(mediaType = "application/json"))
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "알림 전송 성공"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "4xx", description = "알림 전송 실패", content = @Content(mediaType = "application/json"))
 		}
 	)
 	@PostMapping("/send")
 	public ResponseEntity<?> sendNotification(
 		@Valid @RequestBody SendNotificationRequestDto requestDto) {
 		fcmCommandService.sendNotification(requestDto);
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok(ApiResponse.ok(requestDto));
 	}
 }
