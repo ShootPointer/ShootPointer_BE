ea8c7bab144b9613f21114eb14dff55641d1a126
tkv00 <tkv0098@gmail.com>
2025-11-13 01:26:46 +0900

    chore : @ConditionalOnProperty 추가

--- src/main/java/com/midas/shootpointer/global/config/FcmConfig.java
+++ src/main/java/com/midas/shootpointer/global/config/FcmConfig.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +13,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 
 @Configuration
+@ConditionalOnProperty(value = "firebase.enabled", havingValue = "true", matchIfMissing = true)
 public class FcmConfig {
 	
 	@Value("${firebase.service_key.path}")
