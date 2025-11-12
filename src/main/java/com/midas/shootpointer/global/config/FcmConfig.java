ef161d77aab7631652d961a76eb1f8bcf69de8c0
tkv00 <tkv0098@gmail.com>
2025-11-12 23:55:20 +0900

    refact:FcmConfig fcm.enable=true일 때만 동작 가능 수정

--- src/main/java/com/midas/shootpointer/global/config/FcmConfig.java
+++ src/main/java/com/midas/shootpointer/global/config/FcmConfig.java
@@ -4,13 +4,16 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+
 @Configuration
+@ConditionalOnProperty(name = "fcm.enabled", havingValue = "true", matchIfMissing = false)
 public class FcmConfig {
 	
 	@Value("${firebase.service_key.path}")
