2a42b30896f731f9b894c14fb2ec52222e6e5bd4
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-21 02:51:59 +0900

    Update RedisOpenCVConfig.java

--- src/main/java/com/midas/shootpointer/infrastructure/redis/config/RedisOpenCVConfig.java
+++ src/main/java/com/midas/shootpointer/infrastructure/redis/config/RedisOpenCVConfig.java
@@ -26,6 +26,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.RedisPassword;
 
 import java.time.Duration;
 
